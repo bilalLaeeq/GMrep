d5f268327360a3c620f3324f84da7a7083c00341
hassan335 <42171459+hassan335@users.noreply.github.com>
2022-03-11 06:54:50 -0800

    Update Ball.cs

--- LAB01/Ball.cs
+++ LAB01/Ball.cs
@@ -36,3 +36,66 @@ public class Ball : MonoBehaviour
         }
     }
 }
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Ball : MonoBehaviour
+{
+public float speed;
+public Rigidbody2D rb;
+public Vector3 startPosition;
+
+    private float x;
+    private float y;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        Launch();
+    }
+
+    private void Launch()
+    {
+        //   float x = Random.Range(0, 2) == 0 ? -1 : 1;
+
+        if (Random.Range(0, 2) == 0)
+        {
+            x = -1;
+        }
+        else
+        {
+            x = 1;
+        }
+
+        if (Random.Range(0, 2) == 0)
+        {
+            y = -1;
+        }
+        else
+        {
+            y = 1;
+        }
+
+        rb.velocity = new Vector2(speed * x, speed * y);
+    }
+
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero;
+        transform.position = startPosition;
+        Launch();
+    }
+}
+
