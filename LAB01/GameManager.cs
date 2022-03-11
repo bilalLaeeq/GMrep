a11f4a87124615dbf405a20dd1b9fa6616377727
hassan335 <42171459+hassan335@users.noreply.github.com>
2022-03-11 06:55:41 -0800

    Update GameManager.cs

--- LAB01/GameManager.cs
+++ LAB01/GameManager.cs
@@ -25,3 +25,60 @@ public class GameManager : MonoBehaviour
         control.velocity = new Vector2(control.velocity.x, position);
     }
 }
+
+
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Ball")]
+    public GameObject ball;
+
+    [Header("Player 1")]
+    public GameObject player1Paddle;
+    public GameObject player1Goal;
+
+    [Header("Player 2")]
+    public GameObject player2Paddle;
+    public GameObject player2Goal;
+
+    [Header("Score UI")]
+    public GameObject Player1Text;
+    public GameObject Player2Text;
+
+    private int Player1Score;
+    private int Player2Score;
+
+    private void ResetPosition()
+    {
+        ball.GetComponent<Ball>().Reset();
+        //        player1Paddle.GetComponent<Paddle>().Reset();
+        //       player2Paddle.GetComponent<Paddle>().Reset();
+    }
+
+
+    public void Player1Scored()
+    {
+        Player1Score++;
+        Player1Text.GetComponent<TextMeshProUGUI>().text = Player1Score.ToString();
+        ResetPosition();
+    }
+
+    public void Player2Scored()
+    {
+        Player2Score++;
+        Player2Text.GetComponent<TextMeshProUGUI>().text = Player2Score.ToString();
+        ResetPosition();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
